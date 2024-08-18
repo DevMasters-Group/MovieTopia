90954d5e08390cd25f331d4702dc98dd331fada6
LaventelHaan <lock0407.work@gmail.com>
2024-08-18 11:51:30 +0200

    Fixed all except add functionality

--- MovieTopia/HomeStaff.Designer.cs
+++ MovieTopia/HomeStaff.Designer.cs
@@ -29,20 +29,50 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HomeStaff));
+            this.dgvSchedules = new System.Windows.Forms.DataGridView();
+            this.picLogo = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSchedules)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dgvSchedules
+            // 
+            this.dgvSchedules.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSchedules.Location = new System.Drawing.Point(437, 247);
+            this.dgvSchedules.Name = "dgvSchedules";
+            this.dgvSchedules.RowHeadersWidth = 51;
+            this.dgvSchedules.RowTemplate.Height = 24;
+            this.dgvSchedules.Size = new System.Drawing.Size(712, 312);
+            this.dgvSchedules.TabIndex = 0;
+            // 
+            // picLogo
+            // 
+            this.picLogo.Location = new System.Drawing.Point(587, 60);
+            this.picLogo.Name = "picLogo";
+            this.picLogo.Size = new System.Drawing.Size(100, 50);
+            this.picLogo.TabIndex = 1;
+            this.picLogo.TabStop = false;
             // 
             // HomeStaff
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1438, 745);
+            this.Controls.Add(this.picLogo);
+            this.Controls.Add(this.dgvSchedules);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "HomeStaff";
             this.Text = "HomeStaff";
+            this.Load += new System.EventHandler(this.HomeStaff_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSchedules)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvSchedules;
+        private System.Windows.Forms.PictureBox picLogo;
     }
 }
--- MovieTopia/HomeStaff.cs
+++ MovieTopia/HomeStaff.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,61 @@ namespace MovieTopia
 {
     public partial class HomeStaff : Form
     {
+        private string DATABASE_URL;
+        private int padding = 20;
+        DataSet ds;
+        SqlDataAdapter adapter;
         public HomeStaff()
         {
+            // get database connection string
+            DATABASE_URL = Environment.GetEnvironmentVariable("DATABASE_URL");
+
             InitializeComponent();
+
+            // load initial data when form loads
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            using (SqlConnection conn = new SqlConnection(DATABASE_URL))
+            {
+                ds = new DataSet();
+                adapter = new SqlDataAdapter();
+
+                // select the parent table and join any additional fields from child entities
+                string sqlMovieSchedules = @"
+                            SELECT
+                                ms.MovieScheduleID, ms.MovieID, ms.TheatreID, ms.Price, ms.DateTime, m.Title, m.Duration, m.PG_Rating, t.TheatreName, g.GenreName
+                            FROM
+                                MovieSchedule ms
+                            JOIN
+                                Movie m ON ms.MovieID = m.MovieID
+                            JOIN
+                                Theatre t ON ms.TheatreID = t.TheatreID
+                            JOIN
+                                Genre g ON m.GenreID = g.GenreID";
+                // NB: select the ENTIRE child entity and store it in the dataset as well. This is used in the DetailsForm for dropdown boxes
+                string sqlMovies = "SELECT * FROM Movie";
+                string sqlTheatres = "SELECT * FROM Theatre WHERE Active = 1;";
+
+                // important to name the returned data in the dataset with the entity name
+                adapter.SelectCommand = new SqlCommand(sqlMovieSchedules, conn); ;
+                adapter.Fill(ds, "MovieSchedule");
+                adapter.SelectCommand = new SqlCommand(sqlMovies, conn); ;
+                adapter.Fill(ds, "Movie");
+                adapter.SelectCommand = new SqlCommand(sqlTheatres, conn); ;
+                adapter.Fill(ds, "Theatre");
+
+                // fill the datagrid
+                dgvSchedules.DataSource = ds;
+                dgvSchedules.DataMember = "MovieSchedule";
+            }
+        }
+
+        private void HomeStaff_Load(object sender, EventArgs e)
+        {
+            
         }
     }
 }
