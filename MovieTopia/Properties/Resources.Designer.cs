b8eb3be3c396b3776de6d37e3c7b4afe39dd3310
jaden-straw <111179220+jaden-straw@users.noreply.github.com>
2024-08-26 15:38:08 +0200

    Added error icon

--- MovieTopia/Properties/Resources.Designer.cs
+++ MovieTopia/Properties/Resources.Designer.cs
@@ -61,6 +61,16 @@ namespace MovieTopia.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon errorIcon {
+            get {
+                object obj = ResourceManager.GetObject("errorIcon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap logoFullDark {
