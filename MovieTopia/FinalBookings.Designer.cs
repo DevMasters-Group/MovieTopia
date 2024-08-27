3ebb43010842694288151d6eefe82ad26bff13d6
LaventelHaan <lock0407.work@gmail.com>
2024-08-27 13:55:16 +0200

    Final Form Created

--- MovieTopia/FinalBookings.Designer.cs
+++ MovieTopia/FinalBookings.Designer.cs
@@ -28,21 +28,257 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.txtCellNum = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtMovie = new System.Windows.Forms.TextBox();
+            this.txtTheatre = new System.Windows.Forms.TextBox();
+            this.rchSeats = new System.Windows.Forms.RichTextBox();
+            this.btnSave = new MovieTopia.Controls.BTN();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(942, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::MovieTopia.Properties.Resources.logoFullDark;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(204, 100);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.LightBlue;
+            this.groupBox1.Controls.Add(this.txtCellNum);
+            this.groupBox1.Controls.Add(this.txtSurname);
+            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(48, 137);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(843, 225);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ticket Holder Information";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(249, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter the Ticket holder\'s First name: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(21, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(239, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter the Ticket holder\'s Surname: ";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(21, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(255, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Enter the Ticket holder\'s cell number: ";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(304, 47);
+            this.txtName.MaxLength = 25;
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(237, 22);
+            this.txtName.TabIndex = 3;
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(304, 104);
+            this.txtSurname.MaxLength = 25;
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(237, 22);
+            this.txtSurname.TabIndex = 4;
+            // 
+            // txtCellNum
+            // 
+            this.txtCellNum.Location = new System.Drawing.Point(304, 162);
+            this.txtCellNum.MaxLength = 10;
+            this.txtCellNum.Name = "txtCellNum";
+            this.txtCellNum.Size = new System.Drawing.Size(237, 22);
+            this.txtCellNum.TabIndex = 5;
+            this.txtCellNum.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCellNum_KeyPress);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.LightPink;
+            this.groupBox2.Controls.Add(this.rchSeats);
+            this.groupBox2.Controls.Add(this.txtTheatre);
+            this.groupBox2.Controls.Add(this.txtMovie);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(48, 393);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(843, 367);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Movie Information";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(21, 40);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 18);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Selected Movie:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(21, 94);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(123, 18);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Selected Theatre:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(21, 145);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(111, 18);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Selected Seats:";
+            // 
+            // txtMovie
+            // 
+            this.txtMovie.Location = new System.Drawing.Point(196, 37);
+            this.txtMovie.MaxLength = 100;
+            this.txtMovie.Name = "txtMovie";
+            this.txtMovie.ReadOnly = true;
+            this.txtMovie.Size = new System.Drawing.Size(237, 22);
+            this.txtMovie.TabIndex = 5;
+            // 
+            // txtTheatre
+            // 
+            this.txtTheatre.Location = new System.Drawing.Point(196, 91);
+            this.txtTheatre.MaxLength = 100;
+            this.txtTheatre.Name = "txtTheatre";
+            this.txtTheatre.ReadOnly = true;
+            this.txtTheatre.Size = new System.Drawing.Size(237, 22);
+            this.txtTheatre.TabIndex = 6;
+            // 
+            // rchSeats
+            // 
+            this.rchSeats.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rchSeats.Location = new System.Drawing.Point(196, 145);
+            this.rchSeats.Name = "rchSeats";
+            this.rchSeats.ReadOnly = true;
+            this.rchSeats.Size = new System.Drawing.Size(381, 192);
+            this.rchSeats.TabIndex = 7;
+            this.rchSeats.Text = "The seats you have selected are as follows:\n";
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnSave.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnSave.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.btnSave.BorderRadius = 40;
+            this.btnSave.BorderSize = 0;
+            this.btnSave.FlatAppearance.BorderSize = 0;
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.ForeColor = System.Drawing.Color.White;
+            this.btnSave.Location = new System.Drawing.Point(352, 792);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(169, 53);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Book Tickets";
+            this.btnSave.TextColor = System.Drawing.Color.White;
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // FinalBookings
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(790, 529);
+            this.ClientSize = new System.Drawing.Size(942, 857);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
             this.Name = "FinalBookings";
             this.Text = "FinalBookings";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.FinalBookings_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCellNum;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox rchSeats;
+        private System.Windows.Forms.TextBox txtTheatre;
+        private System.Windows.Forms.TextBox txtMovie;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private Controls.BTN btnSave;
     }
 }
--- MovieTopia/FinalBookings.cs
+++ MovieTopia/FinalBookings.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,17 +13,142 @@ namespace MovieTopia
 {
     public partial class FinalBookings : Form
     {
+        private string DATABASE_URL;
+        DataSet ds;
+        SqlDataAdapter adapter;
+
+        private int MovieScheduleID;
+        private List<string> SeatNames;
+
         public FinalBookings(List<string> selectedSeats, int movieScheduleID)
         {
-            InitializeComponent();
+            DATABASE_URL = Environment.GetEnvironmentVariable("DATABASE_URL");
 
-            string selectedSeatsMessage = string.Join(", ", selectedSeats.ToArray());
-            MessageBox.Show(selectedSeatsMessage);
+            InitializeComponent();
+            MovieScheduleID = movieScheduleID;
+            SeatNames = selectedSeats;
+
+            DisplayMovieScheduleDetails();
         }
+
+        private void DisplayMovieScheduleDetails()
+        {
+            using (SqlConnection conn = new SqlConnection(DATABASE_URL))
+            {
+                conn.Open();
+
+                // Pull all info from MovieSchedule table using MovieScheduleID
+                string sqlMovieSchedule = @"
+            SELECT
+                ms.MovieID,
+                ms.TheatreID,
+                ms.DateTime
+            FROM
+                MovieSchedule ms
+            WHERE
+                ms.MovieScheduleID = @MovieScheduleID";
+
+                SqlCommand command = new SqlCommand(sqlMovieSchedule, conn);
+                command.Parameters.AddWithValue("@MovieScheduleID", MovieScheduleID);
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                int movieID = 0;
+                int theatreID = 0;
+                DateTime scheduleDateTime;
+
+                if (reader.Read())
+                {
+                    movieID = Convert.ToInt32(reader["MovieID"]);
+                    theatreID = Convert.ToInt32(reader["TheatreID"]);
+                    scheduleDateTime = Convert.ToDateTime(reader["DateTime"]);
+                }
+                reader.Close();
+
+                // Get the MovieName from the Movie table
+                string sqlMovie = @"
+            SELECT
+                Title
+            FROM
+                Movie
+            WHERE
+                MovieID = @MovieID";
+
+                command = new SqlCommand(sqlMovie, conn);
+                command.Parameters.AddWithValue("@MovieID", movieID);
+
+                string movieName = command.ExecuteScalar().ToString();
+
+                // Get the TheatreName from the Theatre table
+                string sqlTheatre = @"
+            SELECT
+                TheatreName
+            FROM
+                Theatre
+            WHERE
+                TheatreID = @TheatreID";
+
+                command = new SqlCommand(sqlTheatre, conn);
+                command.Parameters.AddWithValue("@TheatreID", theatreID);
+
+                string theatreName = command.ExecuteScalar().ToString();
+
+                // Display the movie name in the textbox - txtMovie
+                txtMovie.Text = movieName;
+
+                // Display the theatre name in the textbox - txtTheatre
+                txtTheatre.Text = theatreName;
+
+                // Add all the seat names to the richtextbox - rchSeats
+                DisplaySeatsInRichTextBox();
+            }
+        }
+
+        private void DisplaySeatsInRichTextBox()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("The Seats you have selected are:\n");
+            sb.AppendLine("Seat Row\t\tSeat Column");
+            sb.AppendLine("========\t\t===========");
+
+            foreach (string seatName in SeatNames)
+            {
+                if (seatName.Length > 1)
+                {
+                    string seatRow = seatName.Substring(0, 1); // First character as the row
+                    string seatNumber = seatName.Substring(1); // Remaining characters as the number
+
+                    sb.AppendLine($"{seatRow}\t\t\t{seatNumber}");
+                }
+            }
+
+            rchSeats.Text = sb.ToString();
+        }
+
+
 
         private void FinalBookings_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtCellNum_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Check if the pressed key is not a digit and not a control key (e.g., backspace)
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true; // Suppress the key press
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Under Construction - Please be patient");
         }
     }
 }
