04bcca29ebff6c6dab07c617ad952c66fbf1bfee
jaden-straw <111179220+jaden-straw@users.noreply.github.com>
2024-08-29 14:47:14 +0200

    Phone number validation

--- MovieTopia/BookingConfirmation.cs
+++ MovieTopia/BookingConfirmation.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -39,7 +40,7 @@ namespace MovieTopia
         {
             txtFName.Validating += new CancelEventHandler(textBox_Validating);
             txtLName.Validating += new CancelEventHandler(textBox_Validating);
-            txtPhoneNumber.Validating += new CancelEventHandler(textBox_Validating);
+            txtPhoneNumber.Validating += new CancelEventHandler(textBoxPhoneNumber_Validating);
         }
 
         private void LoadData()
@@ -111,6 +112,33 @@ namespace MovieTopia
             }
         }
 
+        /// <summary>
+        /// Validates the TextBox value to prevent blank or whitespace entries
+        /// </summary>
+        /// <param name="sender">The control.</param>
+        /// <param name="e">The CancelEventsArgs.</param>
+        private void textBoxPhoneNumber_Validating(object sender, CancelEventArgs e)
+        {
+            TextBox tb = sender as TextBox;
+            Regex expr = new Regex(@"^\d{10}$");
+            if (string.IsNullOrWhiteSpace(tb.Text) || string.IsNullOrEmpty(tb.Text))
+            {
+                errorProvider1.SetIconPadding(tb, padding);
+                errorProvider1.SetError(tb, "This field is required.");
+                e.Cancel = true;
+            }
+            else if (!expr.IsMatch(tb.Text))
+            {
+                errorProvider1.SetIconPadding(tb, padding);
+                errorProvider1.SetError(tb, "Please enter a valid 10-digit phone number.");
+                e.Cancel = true;
+            }
+            else
+            {
+                errorProvider1.SetError(tb, "");
+            }
+        }
+
         private void btnCancel_Click(object sender, EventArgs e)
         {
             DialogResult result = MessageBox.Show("Are you sure you want to cancel your booking?", "Cancel", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
@@ -146,6 +174,15 @@ namespace MovieTopia
             {
                 e.Cancel = true;
                 //MessageBox.Show("Please correct the validation errors before closing the form.");
+            }
+        }
+
+        private void txtPhoneNumber_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            // Check if the pressed key is not a digit and not a control key (e.g., backspace)
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true; // Suppress the key press
             }
         }
     }
