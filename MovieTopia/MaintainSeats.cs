388b5553e94de2276c4fb550e8a7264144cbbab6
LaventelHaan <lock0407.work@gmail.com>
2024-09-01 00:07:40 +0200

    Delete Validation for seats added

--- MovieTopia/MaintainSeats.cs
+++ MovieTopia/MaintainSeats.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Common;
 using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
@@ -230,43 +231,118 @@ namespace MovieTopia
             if (dgvData.SelectedRows.Count == 1)
             {
                 DataGridViewRow selectedRow = dgvData.SelectedRows[0];
+                string seatCol = "EMPTY";
+                int seatRow = 0;
 
-                DialogResult confirm = MessageBox.Show($"Are you sure you want to delete \"{selectedRow.Cells["SeatColumn"].Value}{selectedRow.Cells["SeatRow"].Value}\"?", "Delete Seat", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-                if (confirm == DialogResult.No) return;
+                string sqlSeats = @"SELECT SeatColumn, SeatRow FROM Seat WHERE SeatID = @SeatID";
+                using (SqlConnection connection = new SqlConnection(DATABASE_URL))
+                {
+                    connection.Open();
 
-                string sql = @"
+                    using (SqlCommand command = new SqlCommand(sqlSeats, connection))
+                    {
+                        command.Parameters.AddWithValue("@SeatID", selectedRow.Cells[0].Value);
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read()) 
+                            {
+                                seatCol = reader["SeatColumn"].ToString();
+                                seatRow = Convert.ToInt32(reader["SeatRow"]);
+                                reader.Close();
+                                connection.Close();
+                            }
+                            connection.Close();
+                        }
+                    }
+                }
+
+                if (seatCol != "EMPTY" && seatRow != 0)
+                {
+                    int ColToNum = 0;
+                    
+                    if (seatCol.Length == 2)
+                    {
+                        char letter = seatCol[1];
+                        ColToNum += (letter - 'A' + 27);
+                    }
+                    else
+                    {
+                        char letter = seatCol[0];
+                        ColToNum += (letter - 'A' + 1);
+                    }
+
+                    string sqlCheck = @"SELECT * FROM Theatre WHERE (@SeatCol BETWEEN 1 AND NumCols) AND NumRows >= @SeatRow";
+                    using (SqlConnection connection = new SqlConnection(DATABASE_URL))
+                    {
+                        SqlCommand command = new SqlCommand(sqlCheck, connection);
+                        command.Parameters.AddWithValue("@SeatCol", ColToNum);
+                        command.Parameters.AddWithValue("@SeatRow", seatRow);
+
+                        try
+                        {
+                            connection.Open();
+                            using (SqlDataReader reader = command.ExecuteReader())
+                            {
+
+                                if (reader.HasRows)
+                                {
+                                    MessageBox.Show("You can't delete that seat because it is in use by an active theatre");
+                                    reader.Close();
+                                    connection.Close();
+                                    return;
+                                }
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message, "Error");
+                            connection.Close();
+                            return;
+                        }
+                    }
+
+                    DialogResult confirm = MessageBox.Show($"Are you sure you want to delete \"{selectedRow.Cells["SeatColumn"].Value}{selectedRow.Cells["SeatRow"].Value}\"?", "Delete Seat", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    if (confirm == DialogResult.No) return;
+
+                    string sql = @"
                         DELETE FROM 
                             Seat
                         WHERE
                             SeatID = @SeatID;";
 
-                using (SqlConnection connection = new SqlConnection(DATABASE_URL))
-                {
-                    SqlCommand command = new SqlCommand(sql, connection);
-                    //command.Parameters.Add("@GenreID", SqlDbType.Int);
-                    //command.Parameters["@ID"].Value = customerID;
+                    using (SqlConnection connection = new SqlConnection(DATABASE_URL))
+                    {
+                        SqlCommand command = new SqlCommand(sql, connection);
+                        //command.Parameters.Add("@GenreID", SqlDbType.Int);
+                        //command.Parameters["@ID"].Value = customerID;
 
-                    // Use AddWithValue to assign Demographics.
-                    // SQL Server will implicitly convert strings into XML.
-                    command.Parameters.AddWithValue("@SeatID", selectedRow.Cells["SeatID"].Value);
+                        // Use AddWithValue to assign Demographics.
+                        // SQL Server will implicitly convert strings into XML.
+                        command.Parameters.AddWithValue("@SeatID", selectedRow.Cells["SeatID"].Value);
 
-                    try
-                    {
-                        connection.Open();
-                        command.ExecuteNonQuery();
-                        MessageBox.Show("Deleted Successfully", "Success");
+                        try
+                        {
+                            connection.Open();
+                            command.ExecuteNonQuery();
+                            MessageBox.Show("Deleted Successfully", "Success");
+                        }
+                        catch (SqlException)
+                        {
+                            MessageBox.Show($"\"{selectedRow.Cells["SeatColumn"].Value}{selectedRow.Cells["SeatRow"].Value}\" cannot be deleted because it is being referenced by 1 or more Tickets for movies on display, or up-and-coming movies.", "Error");
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message, "Error");
+                        }
                     }
-                    catch (SqlException)
-                    {
-                        MessageBox.Show($"\"{selectedRow.Cells["SeatColumn"].Value}{selectedRow.Cells["SeatRow"].Value}\" cannot be deleted because it is being referenced by 1 or more Tickets for movies on display, or up-and-coming movies.", "Error");
-                    }
-                    catch (Exception ex)
-                    {
-                        MessageBox.Show(ex.Message, "Error");
-                    }
+
+                    LoadData();
                 }
-
-                LoadData();
+                else
+                {
+                    MessageBox.Show("Please select a Seat to delete.");
+                }
             }
             else
             {
