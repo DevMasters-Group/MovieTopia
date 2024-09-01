327fc6ca906f1a93a31a65aacf5b79ca7b6205f8
jaden-straw <111179220+jaden-straw@users.noreply.github.com>
2024-09-01 08:22:20 +0200

    Font change

--- MovieTopia/FinalBookings.Designer.cs
+++ MovieTopia/FinalBookings.Designer.cs
@@ -87,6 +87,7 @@
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.Location = new System.Drawing.Point(48, 137);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(1021, 225);
@@ -96,46 +97,49 @@
             // 
             // txtCellNum
             // 
-            this.txtCellNum.Location = new System.Drawing.Point(304, 162);
+            this.txtCellNum.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCellNum.Location = new System.Drawing.Point(397, 162);
             this.txtCellNum.MaxLength = 10;
             this.txtCellNum.Name = "txtCellNum";
-            this.txtCellNum.Size = new System.Drawing.Size(237, 22);
+            this.txtCellNum.Size = new System.Drawing.Size(300, 28);
             this.txtCellNum.TabIndex = 5;
             this.txtCellNum.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCellNum_KeyPress);
             // 
             // txtSurname
             // 
-            this.txtSurname.Location = new System.Drawing.Point(304, 104);
+            this.txtSurname.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSurname.Location = new System.Drawing.Point(397, 104);
             this.txtSurname.MaxLength = 25;
             this.txtSurname.Name = "txtSurname";
-            this.txtSurname.Size = new System.Drawing.Size(237, 22);
+            this.txtSurname.Size = new System.Drawing.Size(300, 28);
             this.txtSurname.TabIndex = 4;
             // 
             // txtName
             // 
-            this.txtName.Location = new System.Drawing.Point(304, 47);
+            this.txtName.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(397, 47);
             this.txtName.MaxLength = 25;
             this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(237, 22);
+            this.txtName.Size = new System.Drawing.Size(300, 28);
             this.txtName.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(21, 165);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(255, 18);
+            this.label3.Size = new System.Drawing.Size(310, 21);
             this.label3.TabIndex = 2;
             this.label3.Text = "Enter the Ticket holder\'s cell number: ";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(21, 107);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(239, 18);
+            this.label2.Size = new System.Drawing.Size(290, 21);
             this.label2.TabIndex = 1;
             this.label2.Text = "Enter the Ticket holder\'s Surname: ";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -143,10 +147,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(21, 50);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(249, 18);
+            this.label1.Size = new System.Drawing.Size(302, 21);
             this.label1.TabIndex = 0;
             this.label1.Text = "Enter the Ticket holder\'s First name: ";
             // 
@@ -163,6 +167,7 @@
             this.groupBox2.Controls.Add(this.label6);
             this.groupBox2.Controls.Add(this.label5);
             this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox2.Location = new System.Drawing.Point(48, 393);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(1021, 443);
@@ -172,99 +177,107 @@
             // 
             // txtTotal
             // 
-            this.txtTotal.Location = new System.Drawing.Point(597, 145);
+            this.txtTotal.Enabled = false;
+            this.txtTotal.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTotal.Location = new System.Drawing.Point(645, 145);
             this.txtTotal.MaxLength = 100;
             this.txtTotal.Name = "txtTotal";
             this.txtTotal.ReadOnly = true;
-            this.txtTotal.Size = new System.Drawing.Size(237, 22);
+            this.txtTotal.Size = new System.Drawing.Size(237, 28);
             this.txtTotal.TabIndex = 11;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(491, 148);
+            this.label8.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(510, 148);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(83, 18);
+            this.label8.Size = new System.Drawing.Size(100, 21);
             this.label8.TabIndex = 10;
             this.label8.Text = "Total Price:";
             // 
             // txtPrice
             // 
+            this.txtPrice.Enabled = false;
+            this.txtPrice.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtPrice.Location = new System.Drawing.Point(196, 145);
             this.txtPrice.MaxLength = 100;
             this.txtPrice.Name = "txtPrice";
             this.txtPrice.ReadOnly = true;
-            this.txtPrice.Size = new System.Drawing.Size(237, 22);
+            this.txtPrice.Size = new System.Drawing.Size(237, 28);
             this.txtPrice.TabIndex = 9;
             this.txtPrice.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.Location = new System.Drawing.Point(21, 148);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(117, 18);
+            this.label7.Size = new System.Drawing.Size(142, 21);
             this.label7.TabIndex = 8;
             this.label7.Text = "Price Per Ticket:";
             this.label7.Click += new System.EventHandler(this.label7_Click);
             // 
             // rchSeats
             // 
-            this.rchSeats.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rchSeats.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.rchSeats.Location = new System.Drawing.Point(196, 203);
             this.rchSeats.Name = "rchSeats";
             this.rchSeats.ReadOnly = true;
-            this.rchSeats.Size = new System.Drawing.Size(524, 192);
+            this.rchSeats.Size = new System.Drawing.Size(686, 192);
             this.rchSeats.TabIndex = 7;
             this.rchSeats.Text = "The seats you have selected are as follows:\n";
             // 
             // txtTheatre
             // 
+            this.txtTheatre.Enabled = false;
+            this.txtTheatre.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtTheatre.Location = new System.Drawing.Point(196, 91);
             this.txtTheatre.MaxLength = 100;
             this.txtTheatre.Name = "txtTheatre";
             this.txtTheatre.ReadOnly = true;
-            this.txtTheatre.Size = new System.Drawing.Size(237, 22);
+            this.txtTheatre.Size = new System.Drawing.Size(237, 28);
             this.txtTheatre.TabIndex = 6;
             // 
             // txtMovie
             // 
+            this.txtMovie.Enabled = false;
+            this.txtMovie.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtMovie.Location = new System.Drawing.Point(196, 37);
             this.txtMovie.MaxLength = 100;
             this.txtMovie.Name = "txtMovie";
             this.txtMovie.ReadOnly = true;
-            this.txtMovie.Size = new System.Drawing.Size(237, 22);
+            this.txtMovie.Size = new System.Drawing.Size(237, 28);
             this.txtMovie.TabIndex = 5;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.Location = new System.Drawing.Point(21, 203);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(111, 18);
+            this.label6.Size = new System.Drawing.Size(136, 21);
             this.label6.TabIndex = 2;
             this.label6.Text = "Selected Seats:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.Location = new System.Drawing.Point(21, 94);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(123, 18);
+            this.label5.Size = new System.Drawing.Size(153, 21);
             this.label5.TabIndex = 1;
             this.label5.Text = "Selected Theatre:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(21, 40);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(113, 18);
+            this.label4.Size = new System.Drawing.Size(138, 21);
             this.label4.TabIndex = 0;
             this.label4.Text = "Selected Movie:";
             // 
@@ -277,11 +290,11 @@
             this.btnGoBack.BorderSize = 0;
             this.btnGoBack.FlatAppearance.BorderSize = 0;
             this.btnGoBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnGoBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGoBack.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnGoBack.ForeColor = System.Drawing.Color.White;
             this.btnGoBack.Location = new System.Drawing.Point(576, 870);
             this.btnGoBack.Name = "btnGoBack";
-            this.btnGoBack.Size = new System.Drawing.Size(169, 53);
+            this.btnGoBack.Size = new System.Drawing.Size(180, 50);
             this.btnGoBack.TabIndex = 4;
             this.btnGoBack.Text = "Go Back";
             this.btnGoBack.TextColor = System.Drawing.Color.White;
@@ -297,11 +310,11 @@
             this.btnSave.BorderSize = 0;
             this.btnSave.FlatAppearance.BorderSize = 0;
             this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSave.ForeColor = System.Drawing.Color.White;
             this.btnSave.Location = new System.Drawing.Point(353, 870);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(169, 53);
+            this.btnSave.Size = new System.Drawing.Size(180, 50);
             this.btnSave.TabIndex = 3;
             this.btnSave.Text = "Book Tickets";
             this.btnSave.TextColor = System.Drawing.Color.White;
